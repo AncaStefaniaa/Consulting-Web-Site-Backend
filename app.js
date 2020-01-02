a7a0f6b5b855c8301f1516c72ff676d17a499542
AncaStefaniaa <anca.ilicea@yahoo.com>
2020-01-02 15:00:53 +0200

    7

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
-
+const expressLayouts = require('express-ejs-layouts');
 mongoose.connect('mongodb://localhost/backend');
 let db = mongoose.connection;
 
@@ -17,7 +17,8 @@ db.on('error',function(){
 });
 //init app
 const app = express();
-
+/*app.use(expressLayouts);
+app.set('view engine','ejs');*/
 //bring in models
 let Article = require('./models/article');
 
@@ -38,10 +39,12 @@ app.use(bodyParser.json())
 //get single argicle
 app.get('/article/:id',function(req,res){
     Article.findById(req.params.id,function(err, article){
-        console.log(article);
-        return;
+       res.render('article',{
+          article:article
+       });
     });
 });
+
 
 
 //home route
@@ -85,6 +88,37 @@ app.post('/articles/add',function(req,res){
         }
     });
 });  
+//load edit form
+
+app.get('/article/edit/:id',function(req,res){
+    Article.findById(req.params.id,function(err, article){
+       res.render('edit_article',{
+          title:'edit article',
+          article:article
+       });
+    });
+});
+
+//edit submit
+app.post('/articles/edit/:id',function(req,res){
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id}
+    Article.update(query, article, function(err){
+        if(err){
+            console.log(err);
+            return;
+        }
+        else{
+           res.redirect('/'); 
+        }
+    });
+});  
+
+
 //start server
 app.listen(3000,function(){
     console.log('Server started on port 3000');
